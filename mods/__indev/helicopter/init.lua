44a267d35857ce89c3cc37d2b3531b86de20e9a6
Pilcrow182 <nick@localhost.localdomain>
2018-11-06 04:18:14 -0600

    add entity based helicopter instant-stop function

--- mods/__indev/helicopter/init.lua
+++ mods/__indev/helicopter/init.lua
@@ -1,17 +1,44 @@
 helicopter = {}
 
+helicopter.speed = minetest.settings:get("helicopter_speed") or 8
+minetest.settings:set("helicopter_speed", helicopter.speed)
+
+helicopter.passive_stop = minetest.settings:get_bool("helicopter_passive_stop", false)
+minetest.settings:set_bool("helicopter_passive_stop", helicopter.passive_stop)
+
 helicopter.using_heli = {}
 
-helicopter.speed = tonumber(minetest.setting_get("helicopter_speed"))
-if not helicopter.speed then
-	helicopter.speed = 8
-	minetest.setting_set("helicopter_speed", tostring(helicopter.speed))
-end
+helicopter.stopped = {}
 
 local debug_msg = function(message)
 	minetest.chat_send_all(message)
 	minetest.log("action", message)
 end
+
+local get_state = function(gravity)
+	if gravity < 0 then
+		return "jumping"
+	elseif gravity > 0 then
+		return "sneaking"
+	else
+		return "idle"
+	end
+end
+
+local stopping_player = ""
+minetest.register_entity("helicopter:stopper_entity", {
+	textures = {"helicopter_stopper_entity.png"},
+	collisionbox = {0,0,0,0,0,0},
+	is_visible = true,
+	makes_footstep_sound = false,
+	on_activate = function(self, staticdata)
+		local player = minetest.get_player_by_name(stopping_player)
+		if not player then return end
+		helicopter.stopped[stopping_player] = true
+		player:set_attach(self.object, "", {x=0,y=0,z=0}, {x=0,y=-player:get_look_horizontal()*180/math.pi,z=0})
+		minetest.after(0.2, function(obj) obj:remove() end, self.object)
+	end,
+})
 
 local timer = 0
 minetest.register_globalstep(function(dtime)
@@ -20,28 +47,28 @@ minetest.register_globalstep(function(dtime)
 		timer = 0
 		for name,_ in pairs(helicopter.using_heli) do
 			local player = minetest.get_player_by_name(name)
-			local velocity = player:get_player_velocity()
-			local heli_speed = helicopter.speed
-			local physics = {speed = heli_speed/4, jump = 0, gravity = 0, sneak = false, sneak_glitch = false}
-			local ctrl = player:get_player_control()
+			if player then
+				local ctrl = player:get_player_control()
+				local velocity = player:get_player_velocity()
+				local heli_speed = helicopter.speed
+				local physics = {speed = heli_speed/4, jump = 0, gravity = 0, sneak = false, sneak_glitch = false}
 
-			local player_state = "idle"
-			if ctrl.aux1 then
-				player_state = "stopping"
-				physics.gravity = velocity.y/heli_speed
+				if ctrl.jump then
+					helicopter.stopped[stopping_player] = false
+					physics.gravity = -(heli_speed-velocity.y)/10
+					player:set_physics_override(physics)
+				elseif ctrl.sneak then
+					helicopter.stopped[stopping_player] = false
+					physics.gravity =  (heli_speed+velocity.y)/10
+					player:set_physics_override(physics)
+				elseif (ctrl.aux1 or helicopter.passive_stop) and not helicopter.stopped[name] then
+					stopping_player = name
+					minetest.add_entity(player:get_pos(), "helicopter:stopper_entity")
+					player:set_physics_override(physics)
+				end
 
-			elseif ctrl.jump then
-				player_state = "jumping"
-				physics.gravity = -(heli_speed-velocity.y)/10
-
-			elseif ctrl.sneak then
-				player_state = "sneaking"
-				physics.gravity =  (heli_speed+velocity.y)/10
-
+-- 				debug_msg("player '"..name.."' is "..get_state(physics.gravity).." (physics="..minetest.pos_to_string({x=physics.speed, y=physics.jump,z=physics.gravity})..", velocity="..minetest.pos_to_string(velocity)..")...")
 			end
-
-			player:set_physics_override(physics)
--- 			debug_msg("player '"..name.."' is "..player_state.." (physics="..minetest.pos_to_string({x=physics.speed, y=physics.jump,z=physics.gravity})..", velocity="..minetest.pos_to_string(velocity)..")...")
 		end
 	end
 end)
@@ -76,18 +103,18 @@ minetest.register_craftitem("helicopter:heli", {
 	wield_scale = {x=1, y=1, z=1},
 	liquids_pointable = false,
 	on_use = function(itemstack, user, pointed_thing)
-	local username = user:get_player_name()
-	if helicopter.using_heli[username] then
-		minetest.chat_send_player(username, "Heli mode disabled")
-		user:set_physics_override(helicopter.using_heli[username])
-		helicopter.using_heli[username] = nil
-		if pp and pp.disable then pp.disable[username] = nil end
-	else
-		minetest.chat_send_player(username, "Heli mode enabled")
-		helicopter.using_heli[username] = user:get_physics_override()
-		user:set_physics_override({speed = 2, jump = 0, gravity = 0})
-		if pp and pp.disable then pp.disable[username] = true end
-	end
+		local username = user:get_player_name()
+		if helicopter.using_heli[username] then
+			minetest.chat_send_player(username, "Heli mode disabled")
+			user:set_physics_override(helicopter.using_heli[username])
+			helicopter.using_heli[username] = nil
+			if pp and pp.disable then pp.disable[username] = nil end
+		else
+			minetest.chat_send_player(username, "Heli mode enabled")
+			helicopter.using_heli[username] = user:get_physics_override()
+			user:set_physics_override({speed = 2, jump = 0, gravity = 0, sneak = false, sneak_glitch = false})
+			if pp and pp.disable then pp.disable[username] = true end
+		end
 	end
 })
 
