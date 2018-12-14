b815c3d3e36d97ae8a2786a65d0c97bcfb64e4c0
Pilcrow182 <pilcrowjesus@gmail.com>
2018-12-14 21:21:40 +0000

    integrate shop foundation into schematic to improve speed

--- mods/__extra/shops/building.lua
+++ mods/__extra/shops/building.lua
@@ -8,71 +8,100 @@ local debug_msg = function(message)
 	end
 end
 
-local n01 = { name = "default:desert_sandstone_block" }
-local n02 = { name = "air" }
-local n03 = { name = "stairs:slab_junglewood", param2 = 1 }
-local n04 = { name = "default:desert_stonebrick", param2 = 1 }
-local n05 = { name = "doors:door_wood_b", param2 = 1 }
-local n06 = { name = "doors:hidden" }
-local n07 = { name = "default:glass" }
-local n08 = { name = "default:junglewood", param2 = 1 }
-local n09 = { name = "shops:shopkeeper_upper", param2 = 1 }
-
-local map = {
-	size = {x = 6, y = 5, z = 6},
-	data = {
-		n01, n01, n01, n01, n01, n01,
-		n02, n02, n02, n02, n02, n02,
-		n02, n02, n02, n02, n02, n02,
-		n02, n02, n02, n02, n02, n02,
-		n03, n03, n03, n03, n03, n03,
-
-		n01, n01, n01, n01, n01, n01,
-		n04, n05, n04, n04, n04, n04,
-		n04, n06, n04, n07, n07, n04,
-		n04, n04, n04, n04, n04, n04,
-		n03, n03, n03, n03, n03, n03,
-
-		n01, n01, n01, n01, n01, n01,
-		n04, n02, n02, n08, n02, n04,
-		n04, n02, n02, n02, n09, n04,
-		n04, n02, n02, n02, n02, n04,
-		n03, n03, n03, n03, n03, n03,
-
-		n01, n01, n01, n01, n01, n01,
-		n04, n02, n02, n08, n02, n04,
-		n04, n02, n02, n02, n02, n04,
-		n04, n02, n02, n02, n02, n04,
-		n03, n03, n03, n03, n03, n03,
-
-		n01, n01, n01, n01, n01, n01,
-		n04, n02, n02, n02, n02, n04,
-		n04, n02, n02, n02, n02, n04,
-		n08, n08, n08, n08, n08, n08,
-		n02, n02, n02, n02, n02, n02,
-
-		n01, n01, n01, n01, n01, n01,
-		n04, n04, n04, n04, n04, n04,
-		n04, n04, n04, n04, n04, n04,
-		n03, n03, n03, n03, n03, n03,
-		n02, n02, n02, n02, n02, n02
-	}
-}
-
-shops.spawn_shop = function(pos)
-	debug_msg("[MOD] shops -- building new shop at "..minetest.pos_to_string(pos))
-
-	local fill_node = minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z})
-	for x = 0, map.size.x-1 do
-		for y = 0, map.size.y-1 do
-			for z = 0, map.size.z-1 do
-				minetest.set_node({x = pos.x + x, y = pos.y - map.size.y + y, z = pos.z + map.size.z - 1 - z}, fill_node) -- generate foundation
-			end
-		end
+shops.build_map = function(foundation)
+	if not minetest.registered_nodes[foundation] then
+		foundation = "default:stone"
 	end
 
-	minetest.place_schematic(pos, map, 0, nil, true)
-	minetest.set_node({x = pos.x + 4, y = pos.y + 1, z = pos.z + 2}, {name = "shops:shopkeeper", param2 = 1})
+	local n01 = { name = foundation }
+	local n02 = { name = "default:desert_sandstone_block" }
+	local n03 = { name = "air" }
+	local n04 = { name = "stairs:slab_junglewood", param2 = 1 }
+	local n05 = { name = "default:desert_stonebrick", param2 = 1 }
+	local n06 = { name = "doors:door_wood_b", param2 = 1 }
+	local n07 = { name = "doors:hidden" }
+	local n08 = { name = "default:glass" }
+	local n09 = { name = "default:junglewood", param2 = 1 }
+	local n10 = { name = "shops:shopkeeper", param2 = 1 }
+	local n11 = { name = "shops:shopkeeper_upper", param2 = 1 }
+
+	local map = {
+		size = {x = 6, y = 10, z = 6},
+		data = {
+			n01, n01, n01, n01, n01, n01,
+        	n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n02, n02, n02, n02, n02, n02,
+			n03, n03, n03, n03, n03, n03,
+			n03, n03, n03, n03, n03, n03,
+			n03, n03, n03, n03, n03, n03,
+			n04, n04, n04, n04, n04, n04,
+
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n02, n02, n02, n02, n02, n02,
+			n05, n06, n05, n05, n05, n05,
+			n05, n07, n05, n08, n08, n05,
+			n05, n05, n05, n05, n05, n05,
+			n04, n04, n04, n04, n04, n04,
+
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n02, n02, n02, n02, n02, n02,
+			n05, n03, n03, n09, n10, n05,
+			n05, n03, n03, n03, n11, n05,
+			n05, n03, n03, n03, n03, n05,
+			n04, n04, n04, n04, n04, n04,
+
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n02, n02, n02, n02, n02, n02,
+			n05, n03, n03, n09, n03, n05,
+			n05, n03, n03, n03, n03, n05,
+			n05, n03, n03, n03, n03, n05,
+			n04, n04, n04, n04, n04, n04,
+
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n02, n02, n02, n02, n02, n02,
+			n05, n03, n03, n03, n03, n05,
+			n05, n03, n03, n03, n03, n05,
+			n09, n09, n09, n09, n09, n09,
+			n03, n03, n03, n03, n03, n03,
+
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n01, n01, n01, n01, n01, n01,
+			n02, n02, n02, n02, n02, n02,
+			n05, n05, n05, n05, n05, n05,
+			n05, n05, n05, n05, n05, n05,
+			n04, n04, n04, n04, n04, n04,
+			n03, n03, n03, n03, n03, n03
+		}
+	}
+	return map
+end
+shops.spawn_shop = function(pos)
+	debug_msg("[MOD] shops -- building new shop at "..minetest.pos_to_string(pos))
+	local map = shops.build_map(minetest.get_node({x=pos.x, y=pos.y-2, z=pos.z}).name)
+	minetest.place_schematic({x = pos.x, y = pos.y - 5, z = pos.z}, map, 0, nil, true)
+	minetest.registered_nodes["shops:shopkeeper"].on_construct({x = pos.x + 4, y = pos.y + 1, z = pos.z + 2})
 end
 
 minetest.register_chatcommand("spawn_shop", {
@@ -87,10 +116,11 @@ minetest.register_chatcommand("spawn_shop", {
 })
 
 local valid_spawn = function(pos)
-	debug_msg("[MOD] shops -- checking if pos "..minetest.pos_to_string(pos).."is valid")
+	debug_msg("[MOD] shops -- checking if pos "..minetest.pos_to_string(pos).." is valid")
 	if minetest.get_modpath("wasteland") then return false end -- don't spawn shops when playing dustworld
 	local ignore = minetest.find_node_near(pos, 6, {"ignore"})
 	if ignore then return false end
+	local map = shops.build_map("default:stone")
 	for x = pos.x, pos.x + map.size.x - 1 do
 		for z = pos.z, pos.z + map.size.z - 1 do
 			local node = minetest.get_node({x = x, y = pos.y, z = z})
